96bcc76527438ccc7601420a903852e20fe450b6
Gert-Jan Vercauteren <gert-jan@nfnty.nl>
2018-10-24 15:48:01 +0200

    test: Add test for invalid email and password

--- Example/Tests/CTUserServiceTests.swift
+++ Example/Tests/CTUserServiceTests.swift
@@ -42,6 +42,27 @@ class CTUserServiceTests: QuickSpec {
                         fail("We expect to unwrap")
                     }
                 }
+                
+                it("Handles the error when the username is not a valid email address") {
+                    let url = Bundle(for: type(of: self)).url(forResource: "invalidEmailAndPassword", withExtension: "json")!
+                    let data = try! Data(contentsOf: url)
+                    self.stub(http(.post, uri: "/user"), jsonData(data, status: 422))
+                    
+                    let callToTest = try! CTUserService().create(email: "NOT_VALID_EMAIL", password: "").toBlocking().first()
+                    
+                    if let unWrappedCallToTest = callToTest {
+                        switch unWrappedCallToTest {
+                        case .failure(let error):
+                            expect(error.code) == 422
+                            expect(error.translationKey) == "error.api.validation-failed"
+                            expect(error.description) == "Failed Validation"
+                        default:
+                            fail("We expect errors")
+                        }
+                    } else {
+                        fail("We expect to unwrap")
+                    }
+                }
             }
         }
     }
