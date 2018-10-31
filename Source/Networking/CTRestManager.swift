e5a5208843981b0f6e08f73775c52158561c1846
Gert-Jan Vercauteren <gert-jan@nfnty.nl>
2018-10-31 10:34:11 +0100

    feat: add error handler to completable call (untested)

--- Source/Networking/CTRestManager.swift
+++ Source/Networking/CTRestManager.swift
@@ -61,7 +61,7 @@ public class CTRestManager {
                     case .success:
                         completable(.completed)
                     case .failure:
-                        completable(CTErrorHandler().handle(response: response))
+                        completable(CTErrorHandler().handle(response: response) as! CompletableEvent)
                     }
             }
             
