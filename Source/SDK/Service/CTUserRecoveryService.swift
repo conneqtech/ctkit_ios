1163e682c98d9177d3c08200dfead35be441c69b
Gert-Jan Vercauteren <gert-jan@nfnty.nl>
2018-11-01 11:33:05 +0100

    feat: add SDK for recovery start / finish test: add test for recovery start / finish

--- Source/SDK/Service/CTUserRecoveryService.swift
+++ Source/SDK/Service/CTUserRecoveryService.swift
@@ -22,6 +22,23 @@ public class CTUserRecoveryService: NSObject {
      - Returns: An observable containing success:true in a dictionary, when recovery has been attempted.
      */
     public func recoverUser(email: String) -> Observable<[String: Bool]> {
-        return CTKit.shared.restManager.post(endpoint: "user", parameters:["username":email])
+        return CTKit.shared.restManager.post(endpoint: "user/recover", parameters:["email":email])
+    }
+    
+    
+    /**
+     Finish the password recovery process with the newly chosen password and the reset hash the user received in their emailbox
+     
+     - Precondition: The password needs to match the same conditions as listed in the account creation calls
+     
+     - Parameter password: The new password to be set for the user
+     - Parameter resetHash: Random hash the user received in the email.
+     
+     - Returns: Observable with the result of the reset
+     */
+    public func finishPasswordRecovery(password: String, resetHash: String) -> Observable<[String: Bool]> {
+        return CTKit.shared.restManager.post(endpoint: "user/recover", parameters: [
+            "hash":resetHash,
+            "password":password])
     }
 }
