32fb78ab4464cfbda1eb197e2d74f187574be728
Gert-Jan Vercauteren <gert-jan@nfnty.nl>
2018-11-01 15:00:47 +0100

    test: add  test for bike information search

--- Example/Tests/CTBikeServiceTests.swift
+++ Example/Tests/CTBikeServiceTests.swift
@@ -16,4 +16,28 @@ import RxSwift
 import RxBlocking
 
 class CTBikeServiceTests: QuickSpec {
+    
+    
+    override func spec () {
+        describe("Bike information") {
+            it("searches for a bike with a frame number") {
+                let subjectUnderTest = CTBikeService()
+                
+                self.stub(http(.get, uri: "bike/search"), json([Resolver().getJSONForResource(name: "bike-information")], status: 200))
+                
+                let response = try! subjectUnderTest.searchUnregisteredBike(withFrameIdentifier: "EN15194").toBlocking().first()
+                if let response = response {
+                    let bike = response[0]
+                    
+                    expect(bike.partialIMEI) == "3515640561"
+                    expect(bike.frameNumber) == "EN15194"
+                    
+                    expect(bike.manufacturerDescription) == "Speer Fiets"
+                    expect(bike.manufacturerModelName) == ""
+                    expect(bike.manufacturerProductionDate) == ""
+                    expect(bike.manufacturerSKU) == "9999999"
+                }
+            }
+        }
+    }
 }
