705b36a9211dd7b3e819cf4b82f6c23597aee5e9
Gert-Jan Vercauteren <gert-jan@conneqtech.com>
2018-11-07 09:46:29 +0100

    fix: remove string extension

--- Source/SDK/Service/CTCloudMessagingService.swift
+++ Source/SDK/Service/CTCloudMessagingService.swift
@@ -51,44 +51,4 @@ public class CTCloudMessagingService: NSObject {
         //If there is no bike id, there is no notification to parse for us.
         return nil
     }
-    
-}
-
-
-private extension String {
-    func localized() -> String {
-        // Fetch from database
-        if let fromRealm:Localization = ConneqtechSDK.realm.object(ofType: Localization.self, forPrimaryKey: self), self != fromRealm.value {
-            return fromRealm.value
-        }
-        // Fetch from localized bundle
-        if let path = Bundle.main.path(forResource: Localize.currentLanguage(), ofType: "lproj"), let bundle = Bundle(path: path) {
-            return customLocalized(key: self, bundle: bundle)
-        }
-            // Fetch from base bundle
-        else if let path = Bundle.main.path(forResource: LCLBaseBundle, ofType: "lproj"), let bundle = Bundle(path: path) {
-            return customLocalized(key: self, bundle: bundle)
-        }
-        return self
-    }
-    
-    func localizedFormat(_ arguments: CVarArg...) -> String {
-        return String(format: localized(), arguments: arguments)
-    }
-    
-    func localizedPlural(_ argument: CVarArg) -> String {
-        return NSString.localizedStringWithFormat(localized() as NSString, argument) as String
-    }
-    
-    func customLocalized(key:String, bundle: Bundle) -> String {
-        // Try to fetch from custom table
-        var result = bundle.localizedString(forKey: key, value: nil, table: "Custom")
-        
-        // Fetch from default table
-        if result == key {
-            result = bundle.localizedString(forKey: key, value: nil, table: nil)
-        }
-        
-        return result
-    }
 }
