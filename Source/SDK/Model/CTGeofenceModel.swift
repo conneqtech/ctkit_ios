62322f206f8e8de28d072d55e836021fc10aacfb
Gert-Jan Vercauteren <gert-jan@conneqtech.com>
2019-01-21 13:32:36 +0100

    feat: add init for geofence and lat lon model

--- Source/SDK/Model/CTGeofenceModel.swift
+++ Source/SDK/Model/CTGeofenceModel.swift
@@ -15,6 +15,14 @@ public struct CTGeofenceModel: CTBaseModel {
     public let center:CTLatLonModel
     public let bikeId:Int
 
+    public init(withBikeId bikeId: Int, name: String, radius: Double, center: CTLatLonModel) {
+        self.id = -1
+        self.bikeId = bikeId
+        self.name = name
+        self.radius = radius
+        self.center = center
+    }
+    
     enum CodingKeys: String, CodingKey {
         case id = "id"
         case name = "name"
