9f552a7cd389f70980fb7249c85e72a7345a35d5
Gert-Jan Vercauteren <gert-jan@conneqtech.com>
2019-03-04 14:00:35 +0100

    fix: Remove dummy code

--- Example/Tests/CTServices/CTBikeNotificationSettingsTests.swift
+++ Example/Tests/CTServices/CTBikeNotificationSettingsTests.swift
@@ -18,8 +18,6 @@ class CTBikeNotificationSettingsTests: XCTestCase {
     
     func testFetchNotifications() {
         let keychain = KeychainSwift()
-        keychain.set("dummy", forKey: "accessToken")
-        keychain.set("672f03512ae23fc01c0da0bc0612b81d7044a992", forKey: "refreshToken")
         self.stub(http(.get, uri: "/bike/1"), json(Resolver().getJSONForResource(name: "bike"), status: 200))
         let notificationSettings = try! CTBikeService().fetchNotificationSettings(withBikeId: 1).toBlocking().first()!
         
