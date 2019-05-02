fdeb8d0bfc81e5e8701e1b4aa15d6de08c38ac97
Gert-Jan Vercauteren <gert-jan@conneqtech.com>
2019-05-02 08:28:34 +0200

    feat: Update fields

--- Source/SDK/Model/CTUnregisteredBikeInformationModel.swift
+++ Source/SDK/Model/CTUnregisteredBikeInformationModel.swift
@@ -12,8 +12,11 @@ public struct CTUnregisteredBikeInformationModel: CTBaseModel {
     /// Partial IMEI number the use as a textual hint for the user to input the full IMEI.
     public let partialIMEI: String
 
-    /// The frame number of a bike. The combination of frame_number and imei is needed to register a bike in the bike api. It acts as a second factor authentication during the bike registration process. Without this parameter, bikes cannot be registered by end-users.
+    /// The frame number of a bike.
     public let frameNumber: String
+
+    // The activation code of a bike The combination of activationCode and imei is needed to register a bike in the bike api. It acts as a second factor authentication during the bike registration process. Without this parameter, bikes cannot be registered by end-users.
+    public let activationCode: String
 
     /// The SKU of the bike. When one of the Conneqtech Services must identify the bike type for e.g turning functionality on/off, this parameter will most likely be used
     public let manufacturerSKU: String?
@@ -26,14 +29,21 @@ public struct CTUnregisteredBikeInformationModel: CTBaseModel {
     enum CodingKeys: String, CodingKey {
         case partialIMEI = "imei_first_digits"
         case frameNumber = "frame_number"
+        case activationCode = "activation_code"
         case manufacturerSKU = "art_num"
         case manufacturerModelName = "model_name"
         case registrationFlow = "registration_flow"
     }
 
-    public init(partialIMEI: String, frameNumber: String, manufacturerSKU: String?, modelName: String?, registrationFlow: CTBikeRegistrationFlow) {
+    public init(partialIMEI: String,
+                frameNumber: String,
+                activationCode: String,
+                manufacturerSKU: String?,
+                modelName: String?,
+                registrationFlow: CTBikeRegistrationFlow) {
         self.partialIMEI = partialIMEI
         self.frameNumber = frameNumber
+        self.activationCode = activationCode
         self.manufacturerSKU = manufacturerSKU
         self.manufacturerModelName = modelName
         self.registrationFlow = registrationFlow
