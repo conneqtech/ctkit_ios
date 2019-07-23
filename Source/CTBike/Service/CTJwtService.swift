efd9c86beaafd6646a6f76be50006b0f7298a85a
Gert-Jan Vercauteren <gert-jan@conneqtech.com>
2019-07-23 07:44:11 +0200

    feat: add private jwt service for activity center

---
+++ Source/CTBike/Service/CTJwtService.swift
@@ -0,0 +1,16 @@
+//
+//  CTJwtService.swift
+//  ctkit
+//
+//  Created by Gert-Jan Vercauteren on 23/07/2019.
+//
+
+import Foundation
+import RxSwift
+
+class CTJwtService {
+    func getJwtForUser() -> Observable<String> {
+        return CTKit.shared.restManager.get(endpoint: "jwt/notificationcenter").map { (dict: [String:String]) in
+            return dict["token"]!}
+    }
+}
