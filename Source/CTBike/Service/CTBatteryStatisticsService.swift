127f38263d80a96795d19837311e246e40280bf5
Gert-Jan Vercauteren <gert-jan@conneqtech.com>
2019-09-19 16:18:37 +0200

    feat: Add current battery state

--- Source/CTBike/Service/CTBatteryStatisticsService.swift
+++ Source/CTBike/Service/CTBatteryStatisticsService.swift
@@ -20,5 +20,9 @@ public class CTBatteryStatisticsService: NSObject {
 
         return CTKit.shared.restManager.get(endpoint: "bike/\(identifier)/battery", parameters: parameters)
     }
+
+    public func getCurrentBatteryState(withBikeId identifier: Int) -> Observable<CTCurrentBatteryStateModel> {
+        return CTKit.shared.restManager.get("bike/\'\(identifier)/battery/current-state")
+    }
 }
 
