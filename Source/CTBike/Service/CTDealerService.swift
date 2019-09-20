764515a0de5ab7742ab6dd0ad91e7b06bc6979d5
Gert-Jan Vercauteren <gert-jan@conneqtech.com>
2019-09-20 11:41:36 +0200

    feat: Add base dealer service (dummies)

---
+++ Source/CTBike/Service/CTDealerService.swift
@@ -0,0 +1,49 @@
+//
+//  CTDealerService.swift
+//  ctkit
+//
+//  Created by Gert-Jan Vercauteren on 20/09/2019.
+//
+
+import Foundation
+import RxSwift
+
+public class CTDealerService: NSObject {
+
+    public func fetchAllPaginated(withPage page: Int = 1, limit: Int = 50) -> Observable<CTPaginatedResponseModel<CTDealerModel>> {
+        let parameters: [String:Any] = [
+            "limit": limit,
+            "offset": (page - 1) * limit
+        ]
+
+        return CTKit.shared.restManager.get(endpoint: "dealer", parameters: parameters)
+    }
+
+    public func searchPaginated(withSearchTerm search: String, page: Int = 1, limit: Int = 50) -> Observable<CTPaginatedResponseModel<CTDealerModel>> {
+        let parameters: [String:Any] = [
+            "filter": [
+                "or;name;like;\(search)",
+                "or;reference;like;\(search)",
+                "or;address;like;\(search)",
+                "or;city;like;\(search)",
+                "or;postal_code;like;\(search)",
+            ],
+            "limit": limit,
+            "offset": (page - 1) * limit
+        ]
+
+        return CTKit.shared.restManager.get(endpoint: "dealer", parameters: parameters)
+    }
+
+    public func fetchDealer(withId identfier: Int) -> Observable<CTDealerModel> {
+        return CTKit.shared.restManager.get(endpoint: "dealer/\(identfier)")
+    }
+
+    public func linkDealer(withId identifier: Int, toBike bikeIdentifier: Int) -> Observable<CTDealerModel> {
+        return Observable.empty()
+    }
+
+    public func unlinkDealer(fromBike identifier: Int) -> Completable {
+        return Completable.empty()
+    }
+}
