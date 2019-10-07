aada82b2e5f16fbb89ee6083ee4f1d56d191f1f9
Gert-Jan Vercauteren <gert-jan@conneqtech.com>
2019-10-07 15:59:06 +0200

    feat: Add service for emergency contacts

---
+++ Source/CTBike/Service/CTCrashDetectionService.swift
@@ -0,0 +1,32 @@
+//
+//  CTCrashDetectionService.swift
+//  ctkit
+//
+//  Created by Gert-Jan Vercauteren on 07/10/2019.
+//
+
+import Foundation
+import RxSwift
+
+public class CTCrashDetectionService: NSObject {
+
+    public func setEmergencyContact(withBikeId bikeId: Int, inviteId: String) -> Observable<CTInviteModel> {
+        return setEmergencyContactStatus(withBikeId: bikeId, inviteId: inviteId, enabled: true)
+    }
+
+    public func removeEmergencyContact(withBikeId bikeId: Int, inviteId: String) -> Observable<CTInviteModel> {
+        return setEmergencyContactStatus(withBikeId: bikeId, inviteId: inviteId, enabled: false)
+    }
+
+    public func fetchEmergencyContact(withBikeId bikeId: Int) -> Observable<CTInviteModel?> {
+        return CTShareBikeService().fetchAcceptedInvites(withBikeId: bikeId).map { (response: CTPaginatedResponseModel<CTInviteModel>) in
+            return response.data.filter { $0.isEmergencyContact == true }.first
+        }
+    }
+}
+
+fileprivate extension CTCrashDetectionService {
+    func setEmergencyContactStatus(withBikeId bikeId: Int, inviteId: String, enabled: Bool)  -> Observable<CTInviteModel> {
+        return CTKit.shared.restManager.patch(endpoint: "bike/\(bikeId)/invite/\(inviteId)", parameters: ["is_emergency_contact": enabled])
+    }
+}
