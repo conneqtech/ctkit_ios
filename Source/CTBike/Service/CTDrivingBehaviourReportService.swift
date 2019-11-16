ce89a1a417dd95c844009032d49ffcdebdd6c1ca
Gert-Jan Vercauteren <gert-jan@conneqtech.com>
2019-11-16 14:09:59 +0100

    feat: Change report model

--- Source/CTBike/Service/CTDrivingBehaviourReportService.swift
+++ Source/CTBike/Service/CTDrivingBehaviourReportService.swift
@@ -9,72 +9,73 @@ import Foundation
 import RxSwift
 
 public class CTDrivingBehaviourReportService: NSObject {
-
-    public func fetchBikeReport(withBikeId identifier: Int, from: Date, till: Date) -> Observable<CTDrivingBehaviourReportModel> {
-         return fetchReport(withBikeId: identifier, from: from, till: till)
+    
+    public func fetchBikeReport(withBikeId identifier: Int, from: Date, till: Date, grouping: CTWeeklyReportGroupingType) -> Observable<CTDrivingBehaviourReportModel> {
+        return fetchReport(withBikeId: identifier, from: from, till: till, grouping: grouping)
     }
-
-    public func fetchGlobalReport(from: Date, till: Date) -> Observable<CTDrivingBehaviourReportModel> {
-        return fetchReport(from: from, till: till)
+    
+    public func fetchGlobalReport(from: Date, till: Date,  grouping: CTWeeklyReportGroupingType) -> Observable<CTDrivingBehaviourReportModel> {
+        return fetchReport(from: from, till: till, grouping: grouping)
     }
 }
 
 // MARK: - Private API
 fileprivate extension CTDrivingBehaviourReportService {
-    func fetchReport(withBikeId identifier: Int? = nil, from: Date, till: Date) -> Observable<CTDrivingBehaviourReportModel> {
+    func fetchReport(withBikeId identifier: Int? = nil, from: Date, till: Date, grouping: CTWeeklyReportGroupingType) -> Observable<CTDrivingBehaviourReportModel> {
         let parameters: [String:Any] = [
-           "from": from.toAPIDate(),
-           "till": till.toAPIDate(),
-           "tz": NSTimeZone.system.identifier
+            "from": from.toAPIDate(),
+            "till": till.toAPIDate(),
+            "grouping_type": grouping
         ]
-
+        
         var endpoint = "v2/bike/ride/stats"
-
+        
         if let identifier = identifier {
             endpoint = "v2/bike/\(identifier)/ride/stats"
         }
-
+        
         return CTKit.shared.restManager.get(endpoint: endpoint ,
-           parameters: parameters).map { (response: [CTDrivingBehaviourAPIModel]) in
-               return self.processReportData(data: response)
+                                            parameters: parameters).map { (response: [CTDrivingBehaviourAPIModel]) in
+                                                return self.processReportData(data: response)
         }
     }
-
+    
     func processReportData(data: [CTDrivingBehaviourAPIModel]) -> CTDrivingBehaviourReportModel {
-        var morningRides = 0
-        var afternoonRides = 0
-        var eveningRides = 0
-        var nightRides = 0
-
         var totalRides = 0
-
         var caloriesBurned = 0
         var totalDistance = 0
-
+        var totalShiftAdvice = 0
+        var averagePowerDistribution = 0
+        
+        var groupedItems: [CTDrivingBehaviourGroupedItem] = []
+        
         data.forEach {
-            switch $0.grouping {
-            case .morning:
-                morningRides = $0.numRides
-            case .afternoon:
-                afternoonRides = $0.numRides
-            case .evening:
-                eveningRides = $0.numRides
-            case .night:
-                nightRides = $0.numRides
-            }
-
+            groupedItems.append(CTDrivingBehaviourGroupedItem(
+                rides: $0.numRides,
+                caloriesBurned: $0.caloriesBurned,
+                distance: $0.distance,
+                averagePowerDistribution: $0.averagePowerDistribution,
+                shiftAdvice: $0.shiftAdvice,
+                grouping: $0.grouping)
+            )
+            
             totalRides += $0.numRides
             caloriesBurned += $0.caloriesBurned
             totalDistance += $0.distance
+            totalShiftAdvice += $0.shiftAdvice
+            averagePowerDistribution += $0.averagePowerDistribution
         }
-
-        return CTDrivingBehaviourReportModel(morningRides: morningRides,
-                                             afternoonRides: afternoonRides,
-                                             eveningRides: eveningRides,
-                                             nightRides: nightRides,
-                                             totalRides: totalRides,
-                                             caloriesBurned: caloriesBurned,
-                                             totalDistance: totalDistance
+        
+        // Average over the averages
+        averagePowerDistribution = averagePowerDistribution / data.count
+        
+        return CTDrivingBehaviourReportModel (
+            groupedData: groupedItems,
+            totalRides: totalRides,
+            caloriesBurned: caloriesBurned,
+            totalDistance: totalDistance,
+            averagePowerDistribution: averagePowerDistribution,
+            shiftAdvice: totalShiftAdvice
         )
     }
 }
--- ctkit.podspec
+++ ctkit.podspec
@@ -1,5 +1,5 @@
 Pod::Spec.new do |spec|
-  spec.version          = '1.6.1'
+  spec.version          = '1.7.0'
   spec.summary          = 'SDK to communicate with the connected bike api'
   spec.swift_version    = '5.0'
 
