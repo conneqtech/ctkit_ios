a55b7e4dd727870ae1076aa95fac5670bdc5213e
inigollamosas <inigo.llamosas@conneqtech.com>
2021-01-26 15:44:07 +0100

    fix: make them instance variables for the sake of testing

--- Source/CTBilling/Service/CTSubscriptionService.swift
+++ Source/CTBilling/Service/CTSubscriptionService.swift
@@ -52,11 +52,11 @@ public class CTSubscriptionService: NSObject {
         }
     }
     
-    public static func fetchProducts(withBikeId identifier: Int) -> Observable<[CTProductTypeModel]>{
+    public func fetchProducts(withBikeId identifier: Int) -> Observable<[CTProductTypeModel]>{
         return CTBilling.shared.restManager.get(endpoint: "subscription/bike/\(identifier)/product-type")
     }
 
-    public static func fetchInsuranceProducts(withBikeId identifier: Int) -> Observable<[CTProductTypeModel]> {
+    public func fetchInsuranceProducts(withBikeId identifier: Int) -> Observable<[CTProductTypeModel]> {
         return self.fetchProducts(withBikeId: identifier).map { product in
             product.filter({ $0.productTypeId == .insurance && $0.active })
         }
