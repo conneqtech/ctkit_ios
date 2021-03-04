fbfc4923711bc9b008cdd9eebc8826f1528a5d10
inigollamosas <inigo.llamosas@conneqtech.com>
2021-03-04 14:24:40 +0100

    docs: Write some comments

--- Source/CTBike/Service/CTPhoneAsAGpsService.swift
+++ Source/CTBike/Service/CTPhoneAsAGpsService.swift
@@ -9,16 +9,41 @@
 import Foundation
 import RxSwift
 
+/**
+ The CTPhoneAsAGpsService is the class that interacts with the API for implementing Phone as a Gpgs functionality.
+ */
+
 public class CTPhoneAsAGpsService: NSObject {
 
+    /**
+     Posts payloads for the ongoing ride. The first payload creates the ride on the API
+
+     - Parameter ridePayload: The payload to be sent to the API
+
+     - Parameter bike: The bike you want to create the ride for
+     */
     public func postPayload(ridePayload: CTRidePayloadModel, bike: CTBikeModel) {
         return CTKit.shared.restManager.postUnobserved(endpoint: "v2/bike/\(bike.id)/ride/phone/registerloc", parameters: try? ridePayload.asDictionary())
     }
     
+    /**
+     Ends the ride
+
+     - Parameter bike: The bike you want to end the ride for
+     
+     - Returns: An observable of the newly created ride object
+     */
     public func endRide(bike: CTBikeModel) -> Observable<CTRideModel> {
         return CTKit.shared.restManager.post(endpoint: "v2/bike/\(bike.id)/ride/phone/endride")
     }
     
+    /**
+     Posts metadata of the ongoing ride
+
+     - Parameter bike: The bike you want to record metadata for
+     - Parameter activeTime: The active time in seconds of the ongoing ride
+     - Parameter errorMask: An error indicating what went wrong during the recording: 1 GPS, 2 Bluetooth, 3 GPS + Bluetooth, 4 timeout
+     */
     public func postMetaData(bike: CTBikeModel, activeTime: Int? = nil, errorMask: Int? = nil) {
         var params: [String: Any] = [:]
         if let time = activeTime {
@@ -30,6 +55,15 @@ public class CTPhoneAsAGpsService: NSObject {
         CTKit.shared.restManager.postUnobserved(endpoint: "v2/bike/\(bike.id)/ride/phone/registermeta", parameters: params)
     }
     
+    /**
+     Updates the name and/or the rating of the ride
+
+     - Parameter ride: The ride you want to update
+     - Parameter name: The new name for the ride
+     - Parameter rating: the new rating of the ride (1 to 5). It can also be unset
+     
+     - Returns: An observable of the updated ride object
+     */
     public func patchNameRating(toRide ride: CTRideModel, name: String? = nil, rating: Int? = nil) -> Observable<CTRideModel> {
         var params: [String: Any] = [:]
         if let n = name {
