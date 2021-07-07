469c5f2707a48d8a3a38ad6dd8d91b042b57b2b1
inigollamosas <inigo.llamosas@conneqtech.com>
2021-07-07 16:41:30 +0200

    feat: delete unnecessary code

--- Source/CTCore/Networking/CTIdsAuthManager.swift
+++ Source/CTCore/Networking/CTIdsAuthManager.swift
@@ -12,11 +12,7 @@ import AppAuth
 
 public class CTIdsAuthManager: NSObject {
 
-    private var authState: OIDAuthState?
     public var currentAuthorizationFlow: OIDExternalUserAgentSession? = nil
-    
-    public let state = UUID().uuidString
-    
     var idsTokenApiUrl = ""
     var idsLoginApiUrl = ""
     var idsRedirectUri = ""
@@ -41,33 +37,5 @@ public class CTIdsAuthManager: NSObject {
                                                       tokenType: tokenType)
             
         CTKit.shared.authManager.saveTokenResponse(credentialResponse)
-    }
-    
-    public func createRedirectUrl(clientId: String) -> URL? {
-        
-
-        
-        guard let regionCode = Locale.current.regionCode,
-              let languageCode = Locale.current.languageCode else { return nil }
-        
-        let locale = "\(languageCode)_\(regionCode)"
-        
-        let queryItems = [URLQueryItem(name: "client_id", value: clientId), URLQueryItem(name: "redirect_uri", value: self.idsRedirectUri), URLQueryItem(name: "state", value: self.state), URLQueryItem(name: "locale", value: locale)]
-        if var urlComps = URLComponents(string: "\(self.idsLoginApiUrl)/login") {
-            urlComps.queryItems = queryItems
-            return urlComps.url
-        }
-        return nil
-    }
-    
-    public func login(authorizationCode: String) -> Observable<Any> {
-
-        let parameters = ["grant_type": "authorization_code",
-                          "client_id": CTKit.shared.authManager.apiConfig.clientId,
-                          "client_secret": CTKit.shared.authManager.apiConfig.clientSecret,
-                          "code": authorizationCode,
-                          "redirect_uri": self.idsRedirectUri]
-        
-        return CTKit.shared.authManager.login(url: self.idsTokenApiUrl, parameters: parameters)
     }
 }
