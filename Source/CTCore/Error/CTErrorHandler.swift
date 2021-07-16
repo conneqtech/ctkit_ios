c77fe251c90e7d17b613056089483a43bcf03fa7
inigollamosas <inigo.llamosas@conneqtech.com>
2021-07-16 15:52:33 +0200

    feat: filter out non fatal errors

--- Source/CTCore/Error/CTErrorHandler.swift
+++ Source/CTCore/Error/CTErrorHandler.swift
@@ -63,9 +63,11 @@ internal class CTErrorHandler: NSObject {
         if(!Connectivity.isConnectedToInternet){
             return CTErrorHandler().handleNoInternet()
         }
-
-        let errorInfo = error.getInfoFromResponse(response)
-        NotificationCenter.default.post(name: Notification.Name("logErrorRequest"), object: nil, userInfo: errorInfo)
+        
+        if self.isErrorValid(error) {
+            let errorInfo = error.getInfoFromResponse(response)
+            NotificationCenter.default.post(name: Notification.Name("logErrorRequest"), object: nil, userInfo: errorInfo)
+        }
         
         guard let jsonData = try? JSONSerialization.jsonObject(with: response.data!, options: []) as? [String: Any] else {
 
@@ -80,6 +82,24 @@ internal class CTErrorHandler: NSObject {
         return self.handle(withJSONData: jsonData)
     }
 
+    private func isErrorValid(_ error: Error) -> Bool {
+        
+        guard let errorCode = (error as NSError).code as? Int32 else { return false }
+
+        // 3 is trying to modify a demo account, unauthorized to perform action
+        // 53 is https://developer.apple.com/forums/thread/106838 apple bug for iOS 12
+        // Rest are network errors https://developer.apple.com/documentation/foundation/1448136-nserror_codes
+        // -1200 is a SSL relarted NSURLErrorDomain network error
+        return ![3,
+                53,
+                CFNetworkErrors.cfurlErrorCancelled.rawValue,
+                CFNetworkErrors.cfurlErrorTimedOut.rawValue,
+                CFNetworkErrors.cfurlErrorCannotFindHost.rawValue,
+                CFNetworkErrors.cfurlErrorCannotConnectToHost.rawValue,
+                CFNetworkErrors.cfurlErrorNetworkConnectionLost.rawValue,
+                -1200].contains(errorCode)
+    }
+    
     // Specialized handler functions
     func handleUnauthorized(body: [String: Any]) -> CTBasicError? {
         if let detail = body["detail"] as? String {
--- ctkit.podspec
+++ ctkit.podspec
@@ -1,5 +1,5 @@
 Pod::Spec.new do |spec|
-  spec.version          = '1.28.0'
+  spec.version          = '1.30.0'
   spec.summary          = 'SDK to communicate with the connected bike api'
   spec.swift_version    = '5.0'
 
