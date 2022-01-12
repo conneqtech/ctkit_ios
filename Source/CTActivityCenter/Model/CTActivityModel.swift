0aa00d8980f3f9c1727ec8145c73b9351301d5fb
inigollamosas <inigo.llamosas@conneqtech.com>
2022-01-12 11:59:29 +0100

    feat: init all stored properties

--- Source/CTActivityCenter/Model/CTActivityModel.swift
+++ Source/CTActivityCenter/Model/CTActivityModel.swift
@@ -29,7 +29,34 @@ public struct CTActivtyModel: CTBaseModel {
     public let language: String?
     public let creationDate: Date
 
-    public init(creationDate: Date) {
+    public init(
+        id: String,
+        header: String,
+        headerArgs: [String],
+        subHeader: String,
+        subHeaderArgs: [String],
+        text: String? = nil,
+        textArgs: [String],
+        button: CTActivityButtonModel? = nil,
+        iconUrl: String? = nil,
+        isFeatured: Bool,
+        isDismissable: Bool,
+        language: String? = nil,
+        creationDate: Date
+    
+    ) {
+        self.id = id
+        self.header = header
+        self.headerArgs = headerArgs
+        self.subHeader = subHeader
+        self.subHeaderArgs = subHeaderArgs
+        self.text = text
+        self.textArgs = textArgs
+        self.button = button
+        self.iconUrl = iconUrl
+        self.isFeatured = isFeatured
+        self.isDismissable = isDismissable
+        self.language = language
         self.creationDate = creationDate
     }
     
