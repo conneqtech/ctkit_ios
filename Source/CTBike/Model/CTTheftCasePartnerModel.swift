7b98ad1fc786705ebb3ce6df411daf08308ac2e7
inigollamosas <inigo.llamosas@conneqtech.com>
2022-08-02 11:54:18 +0200

    feat: we dont need CTTheftCasePartnerDescriptionModel on tc 2.0, so is a cleanup

--- Source/CTBike/Model/CTTheftCasePartnerModel.swift
+++ Source/CTBike/Model/CTTheftCasePartnerModel.swift
@@ -7,37 +7,6 @@
 
 import Foundation
 
-public struct CTTheftCasePartnerDescriptionModel: CTBaseModel {
-    public let en: String
-    public let de: String
-    public let nl: String
-    public let da: String
-    public let sv: String
-    public let fi: String
-    public let fr: String
-    public let es: String
-    public let it: String
-    public let cs: String
-    public let pt: String
-    public let pl: String
-    public let hr: String
-    
-    public init(en: String, de: String, nl: String, da: String, sv: String, fi: String, fr: String, es: String, it: String, cs: String, pt: String, pl: String, hr: String) {
-        self.en = en
-        self.de = de
-        self.nl = nl
-        self.da = da
-        self.sv = sv
-        self.fi = fi
-        self.fr = fr
-        self.es = es
-        self.it = it
-        self.cs = cs
-        self.pt = pt
-        self.pl = pl
-        self.hr = hr
-    }
-}
 
 public struct CTTheftCasePartnerModel: CTBaseModel {
     public let id: Int
@@ -49,9 +18,7 @@ public struct CTTheftCasePartnerModel: CTBaseModel {
     public let city: String
     public let country: String
     public let logo: String
-    public let description: CTTheftCasePartnerDescriptionModel
     public var filesTheftReport: Bool?
-    public var partnerActionDescription: String?
     
 
      enum CodingKeys: String, CodingKey {
@@ -64,9 +31,7 @@ public struct CTTheftCasePartnerModel: CTBaseModel {
         case city = "city"
         case country = "country"
         case logo = "logo"
-        case description = "descriptions"
         case filesTheftReport = "files_theft_report"
-        case partnerActionDescription = "partner_action_description"
     }
     
     public init(
@@ -79,9 +44,7 @@ public struct CTTheftCasePartnerModel: CTBaseModel {
         city: String = "",
         country: String = "",
         logo: String = "",
-        description: CTTheftCasePartnerDescriptionModel,
-        filesTheftReport: Bool? = nil,
-        partnerActionDescription: String? = nil) {
+        filesTheftReport: Bool? = nil) {
 
         self.id = id
         self.name = name
@@ -92,8 +55,6 @@ public struct CTTheftCasePartnerModel: CTBaseModel {
         self.city = city
         self.country = country
         self.logo = logo
-        self.description = description
         self.filesTheftReport = filesTheftReport
-        self.partnerActionDescription = partnerActionDescription
     }
 }
