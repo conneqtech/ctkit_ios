e3b155602a03ba49d540407e2f7d9297e6a5da60
inigollamosas <inigo.llamosas@conneqtech.com>
2022-10-19 15:52:50 +0200

    feat: get subs

--- Source/CTExternal/Service/CTNewSubscriptionService.swift
+++ Source/CTExternal/Service/CTNewSubscriptionService.swift
@@ -9,14 +9,8 @@ import Foundation
 import RxSwift
 
 
-
 public class CTNewSubscriptionService: NSObject {
-
-    public func fetchSubscriptions(forImei: String) -> Observable<[CTNewSubscriptionModel]>{
-        
-        CTKit.shared.restManager.get(endpoint: <#T##String#>)
-        
-        
-        return CTBilling.shared.restManager.get(endpoint: "subscription/bike/\(identifier)/product-type")
+    public func fetchSubscriptions(url: String, token: String) -> Observable<[CTNewSubscriptionModel]> {
+        return CTKit.shared.restManager.getGenericUrl(url: url, useToken: token)
     }
 }
