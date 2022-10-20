09e000e1590a7ef87ba505133e0bcb5fc18740d2
inigollamosas <inigo.llamosas@conneqtech.com>
2022-10-20 13:34:13 +0200

    feat: wip convert to date

--- Source/CTExternal/Model/CTNewSubscriptionModel.swift
+++ Source/CTExternal/Model/CTNewSubscriptionModel.swift
@@ -8,10 +8,24 @@
 import Foundation
 
 public struct CTSubscriptionStatusModel: CTBaseModel {
-    public let feature: String
-    public let startDate: String
-    public let endDate: String?
-    public let cancelDate: String?
+    
+    public let feature: String?
+    public let startDateString: String?
+    public var startDate: Date? {
+        get {
+            return self.startDateString?.fromAPIDate()
+        }
+    }
+    public let endDateString: String?
+//    public let endDate: Date?
+    public let cancelDateString: String?
+//    public let cancelDate: Date?e
+    enum CodingKeys: String, CodingKey {
+        case feature
+        case startDateString = "startDate"
+        case endDateString = "endDate"
+        case cancelDateString = "cancelDate"
+    }
 }
 
 public struct CTNewSubscriptionModel: CTBaseModel {
