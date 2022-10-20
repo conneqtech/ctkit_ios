9b443007a21c0b05207e1a40fc4a38fb0e599342
inigollamosas <inigo.llamosas@conneqtech.com>
2022-10-20 12:02:43 +0200

    feat: works

--- Source/CTExternal/Model/CTNewSubscriptionModel.swift
+++ Source/CTExternal/Model/CTNewSubscriptionModel.swift
@@ -12,18 +12,8 @@ public struct CTSubscriptionStatusModel: CTBaseModel {
     public let startDate: String
     public let endDate: String?
     public let cancelDate: String?
-//    enum CodingKeys: String, CodingKey {
-//        case feature
-//        case startDate = "start_date"
-//        case endDate = "end_date"
-//        case cancelDate = "cancel_date"
-//    }
 }
 
 public struct CTNewSubscriptionModel: CTBaseModel {
     public let status: CTSubscriptionStatusModel?
-
-//    enum CodingKeys: String, CodingKey {
-//        case status
-//    }
 }
