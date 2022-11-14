e37bab14f10097194c368340a476e12c03965e91
inigollamosas <inigo.llamosas@conneqtech.com>
2022-11-14 17:17:01 +0100

    feat: decode partner

--- Source/CTBike/Model/CTTheftCasePartnerModel.swift
+++ Source/CTBike/Model/CTTheftCasePartnerModel.swift
@@ -9,15 +9,15 @@ import Foundation
 
 
 public struct CTTheftCasePartnerModel: CTBaseModel {
-    public let id: Int
-    public let name: String
-    public let email: String
-    public let phoneNumber: String
-    public let address: String
-    public let postalCode: String
-    public let city: String
-    public let country: String
-    public let logo: String
+    public let id: Int?
+    public let name: String?
+    public let email: String?
+    public let phoneNumber: String?
+    public let address: String?
+    public let postalCode: String?
+    public let city: String?
+    public let country: String?
+    public let logo: String?
     public var filesTheftReport: Bool?
     public var hasRecovery: Bool?
     
@@ -59,5 +59,24 @@ public struct CTTheftCasePartnerModel: CTBaseModel {
         self.country = country
         self.logo = logo
         self.filesTheftReport = filesTheftReport
+    }
+    
+    
+    public init (from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try container.decode(Int?.self, forKey: .id)
+        name = try container.decode(String?.self, forKey: .name)
+        email = try container.decode(String?.self, forKey: .email)
+        
+        phoneNumber = try container.decode(String?.self, forKey: .phoneNumber)
+        address = try container.decode(String?.self, forKey: .address)
+        postalCode = try container.decode(String?.self, forKey: .postalCode)
+        city = try container.decode(String?.self, forKey: .city)
+        country = try container.decode(String?.self, forKey: .country)
+        logo = try container.decode(String?.self, forKey: .logo)
+        filesTheftReport = try container.decode(Bool?.self, forKey: .filesTheftReport)
+        hasRecovery = try container.decode(Bool?.self, forKey: .hasRecovery)
+    
     }
 }
