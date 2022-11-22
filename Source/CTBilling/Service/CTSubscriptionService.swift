42e8dc54eb11cf71cd921edb5aaf3d09bc6b5d1e
inigollamosas <inigo.llamosas@conneqtech.com>
2022-11-22 10:30:20 +0100

    feat: new endpoint for claim

--- Source/CTBilling/Service/CTSubscriptionService.swift
+++ Source/CTBilling/Service/CTSubscriptionService.swift
@@ -43,9 +43,8 @@ public class CTSubscriptionService: NSObject {
         if !bike.isRequestingUserOwner {
             return Completable.empty()
         } else {
-            return CTBilling.shared.restManager.postCompletable(endpoint: "claim", parameters: [
-                "bike_id": bike.id,
-                "hash": bike.imei
+            return CTBilling.shared.restManager.postCompletable(endpoint: "v1/tools/start-all-in-period", parameters: [
+                "bike_id": bike.id
             ])
         }
     }
