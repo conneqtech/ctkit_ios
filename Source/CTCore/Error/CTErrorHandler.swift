0d6fba70f8d4a73e5d971dafae70eec4adefaf09
inigollamosas <inigo.llamosas@conneqtech.com>
2025-02-24 15:20:32 +0100

    feat: reportableService

--- Source/CTCore/Error/CTErrorHandler.swift
+++ Source/CTCore/Error/CTErrorHandler.swift
@@ -61,11 +61,7 @@ class CTErrorHandler: NSObject {
 
     func handle(response: DataResponse<Any>, error: Error, url: String, reportableService: Bool = false) -> CTErrorProtocol {
 
-        if !reportableService {
-            return
-        }
-
-        if self.isErrorReportable(error, response: response) {
+        if self.isErrorReportable(error, response: response && reportableService) {
             var errorInfo = error.getInfoFromResponse(response)
             errorInfo["url"] = url
             NotificationCenter.default.post(name: Notification.Name("logErrorRequest"), object: nil, userInfo: errorInfo)
