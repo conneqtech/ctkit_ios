4f2afe279a21bf8ba30090778ae2d3401b5ca502
inigollamosas <inigo.llamosas@conneqtech.com>
2025-02-24 15:03:55 +0100

    feat: fix calls

--- Source/CTExternal/Service/CTNewSubscriptionService.swift
+++ Source/CTExternal/Service/CTNewSubscriptionService.swift
@@ -11,10 +11,10 @@ import RxSwift
 public class CTNewSubscriptionService: NSObject {
 
     fileprivate func fetchSubscriptionWrappers(url: String, token: String, headers: [String: String] = [:]) -> Observable<[CTNewSubscriptionWrapperModel]> {
-        return CTKit.shared.restManager.getGenericUrl(url: url, useToken: token, additionalHeaders: headers)
+        return CTKit.shared.restManager.getGenericUrl(url: url, useToken: token, additionalHeaders: headers, reportableService: true)
     }
 
-    public func fetchSubscriptions(imei: String, url: String, token: String, headers: [String: String] = [:], reportableService: true) -> Observable<[CTNewSubscriptionModel]> {
+    public func fetchSubscriptions(imei: String, url: String, token: String, headers: [String: String] = [:]) -> Observable<[CTNewSubscriptionModel]> {
     
         return self.fetchSubscriptionWrappers(url: url, token: token, headers: headers).map { result in
             var allSubscriptions: [CTNewSubscriptionModel] = []
